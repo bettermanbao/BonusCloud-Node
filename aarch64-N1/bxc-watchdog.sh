fbbb1abe7596e194e0401364cfb2530ccd6306d1
bettermanbao <17328315+bettermanbao@users.noreply.github.com>
2019-02-01 22:20:20 +0800

    Update bxc-watchdog.sh

--- aarch64-N1/bxc-watchdog.sh
+++ aarch64-N1/bxc-watchdog.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+/usr/bin/logger -t bxc-watchdog "Start bxc-watchdog"
+
 cnt=0
 while true; do
 	BXC_GATEWAY="fdff:$(ifconfig tun0 | grep fdff | cut -d : -f 3,4,5,6,7,8):1"
 	ping6 -c 1 $BXC_GATEWAY
 	if [ "$?" != "0" ]; then
 		cnt=$((cnt+1))
-		/usr/bin/logger -t bxc-network "Lost connection($cnt)!!!"
+		/usr/bin/logger -t bxc-watchdog "Lost connection($cnt)!!!"
 	fi
 	sleep 60
 done
